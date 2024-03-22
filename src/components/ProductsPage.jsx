9f8782a3ee4a4bb5c7eaa7cf2616d157b4724734
piyushvermaa <piyush.verma_cs22@gla.ac.in>
2024-03-22 10:12:22 +0530

    bug fix

--- src/components/ProductsPage.jsx
+++ src/components/ProductsPage.jsx
@@ -3,8 +3,8 @@ import ProductCard from './ProductCard'
 
 export default function ProductsPage() {
   return (
-    <div className='flex flex-col justify-center'>
-    <h1 className=' text-5xl text-red-800 text-center font-extrabold'>Check Out Our Products</h1>
+    <div className='flex flex-col justify-center mt-10'>
+    <h1 className=' text-5xl text-red-700 text-center  mt-10 font-extrabold'>Check Out Our Products</h1>
     <div className=' flex flex-wrap justify-center'>
       <ProductCard/>
       <ProductCard/>
